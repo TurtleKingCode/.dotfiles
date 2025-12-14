6ec361c50810a0d869200e3d90b1435ca5333dde
Franklin Oguama <turtle.foguama@gmail.com>
2025-12-13 22:50:13 -0500

    refactor(nvim): clean out junk code

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -14,6 +14,7 @@ vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
 	end,
 })
 
+-- Floating box on cursor hold
 vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
 	group = vim.api.nvim_create_augroup('float_diagnostic', { clear = true }),
 	callback = function()
@@ -24,22 +25,7 @@ vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
 	end,
 })
 
--- local file_path = vim.fn.stdpath('data') .. '/last_colorscheme.lua'
---
--- local save_colorscheme = function(colorscheme)
---   vim.uv.fs_open(file_path, "w", 432, function(_, fd)
---     local string_to_write = "return " .. "'" .. colorscheme .. "'"
---     vim.uv.fs_write(fd, string_to_write, nil, function()
---       vim.uv.fs_close(fd)
---     end)
---   end)
--- end
---
--- save_colorscheme(vim.g.colors_name)
---
--- Compute path portably
-
--- Manage State
+-- Persist UI state on exit
 local state_file = vim.fn.stdpath('config') .. '/lua/config/state.lua'
 
 vim.api.nvim_create_autocmd('VimLeavePre', {
--- nvim/lua/config/color.lua
+++
@@ -1,1 +0,0 @@
-return { colorscheme = 'darkblue' }
