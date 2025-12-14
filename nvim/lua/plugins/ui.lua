90327a2b6a979684f7e8d180c128652d50f51a3a
Franklin Oguama <turtle.foguama@gmail.com>
2025-12-14 03:10:13 -0500

    refactor(nvim): clean xiyaowong/transparent.nvim config code thanks to better understanding of their defaults

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -7,25 +7,11 @@ return {
 		'xiyaowong/transparent.nvim',
 		lazy = false,
 		name = 'xiyaowong-transparent.nvim',
-		enabled = true,
 		-- Optional, you don't have to run setup.
-		opts = {
-			-- require('transparent').setup({
-			-- table: default groups
-            -- stylua: ignore
-			groups = {
-				'Normal', 'NormalNC', 'Comment', 'Constant', 'Special',
-				'Identifier', 'Statement', 'PreProc', 'Type', 'Underlined',
-				'Todo', 'String', 'Function', 'Conditional', 'Repeat',
-				'Operator', 'Structure', 'LineNr', 'NonText', 'SignColumn',
-				'CursorLine', 'CursorLineNr', 'StatusLine', 'StatusLineNC', 'EndOfBuffer',
-			},
-			extra_groups = {},
-			exclude_groups = {},
-			on_clear = function() end,
-		},
+		opts = {},
 		config = function(_, opts)
 			require('transparent').setup(opts)
+			require('transparent').clear_prefix('lualine')
 		end,
 		keys = { { '<leader>tr', '<cmd>TransparentToggle<cr>', desc = 'Toggle Transparency' } },
 	},
